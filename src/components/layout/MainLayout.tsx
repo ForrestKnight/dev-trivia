af9fc0baffea93662edce5783c5e6a554d0293e9
ForrestKnight <forrest.knight.yt@gmail.com>
2024-10-17 20:35:05 -0400

    Move chat to the right

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,14 +10,18 @@ export default function MainLayout() {
     <div className="min-h-screen flex flex-col bg-palette-brown">
       <Header />
       <div className="container mx-auto grow bg-palette-brown flex flex-col text-white" style={{ minHeight: 'calc(99vh - 64px)' }}>
-        <div className="flex-grow">
-          <Outlet />
-        </div>
-        {showChat && (
-          <div className="sm:mx-8 border border-white max-w-lg mb-2">
-            <Chat />
+        <div className="flex-grow flex">
+          <div className={`flex-grow ${showChat ? 'w-1/2' : 'w-full'}`}>
+            <Outlet />
           </div>
-        )}
+          {showChat && (
+            <div className="w-1/2 pl-20">
+              <div className="sm:mx-8 border border-white max-w-lg mt-14 mb-2">
+                <Chat />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
